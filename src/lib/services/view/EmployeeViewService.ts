056629bab17e837d62e94cfc478564b10e6c7632
Petr Tripolsky <tripolskypetr@gmail.com>
2024-01-06 00:22:29 +0500

    employee-history

--- src/lib/services/view/EmployeeViewService.ts
+++ src/lib/services/view/EmployeeViewService.ts
@@ -1,12 +1,20 @@
 import EmployeeDbService, { IEmployeeDto } from "../db/EmployeeDbService";
 
+import HistoryViewService from "./HistoryViewService";
 import LoggerService from "../base/LoggerService";
 import Paginator from "../../types/Paginator";
+import PocketbaseService from "../base/PocketbaseService";
 import TYPES from "../../types";
 import { inject } from "react-declarative";
 import { makeObservable } from "mobx";
 
 export class EmployeeViewService {
+  private readonly pocketbaseService = inject<PocketbaseService>(
+    TYPES.pocketbaseService
+  );
+  private readonly historyViewService = inject<HistoryViewService>(
+    TYPES.historyViewService
+  );
   private readonly employeeDbService = inject<EmployeeDbService>(
     TYPES.employeeDbService
   );
@@ -32,16 +40,35 @@ export class EmployeeViewService {
 
   create = async (dto: IEmployeeDto) => {
     this.loggerService.log("employeeViewService create", dto);
-    return await this.employeeDbService.create(dto);
+    const result = await this.employeeDbService.create(dto);
+    this.historyViewService.create({
+      employee_id: result.id,
+      user_id: this.pocketbaseService.userId,
+      type: "employee",
+      comment: "Employee created",
+    });
+    return result;
   };
 
   toggleActive = async (id: string) => {
     this.loggerService.log("employeeViewService toggleActive", { id });
+    this.historyViewService.create({
+      employee_id: id,
+      user_id: this.pocketbaseService.userId,
+      type: "employee",
+      comment: "Archive flag toggled",
+    });
     return await this.employeeDbService.toggleActive(id);
   };
 
   update = async (id: string, dto: IEmployeeDto) => {
     this.loggerService.log("employeeViewService update", dto);
+    this.historyViewService.create({
+      employee_id: id,
+      user_id: this.pocketbaseService.userId,
+      type: "employee",
+      comment: "Employee updated",
+    });
     return await this.employeeDbService.update(id, dto);
   };
 
